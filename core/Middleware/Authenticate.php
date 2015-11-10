6d0500fecab783097b00af0b276915bf29e2ed82
Fernando Marquardt <fernando.marquardt@gmail.com>
2015-11-10 21:06:44 -0200

    Pequenas alterações.

---
+++ core/Middleware/Authenticate.php
@@ -0,0 +1,11 @@
+<?php
+namespace GaleriaBrasil\Middleware;
+
+use \Psr\Http\Message;
+
+class Authenticate {
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next) {
+
+    }
+}
--- public_html/api.php
+++ public_html/api.php
@@ -1,4 +1,5 @@
 <?php
 require '../vendor/autoload.php';
 
-$app = new Slim\Slim();
+$app = new \Slim\App();
+$app->add(new \GaleriaBrasil\Middleware\Authenticate());
